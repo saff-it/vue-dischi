e60cf39e827329ef2d00d9d2c8b5de4e49d32bb6
saff-it <saff_it@ymail.com>
2022-07-19 15:19:25 +0200

    Vue - sass loader not working

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,8 @@ export default {
 }
 </script>
 
-<style>
+
+<style lang="scss">
 
 
 
--- src/components/spotiHeader.vue
+++ src/components/spotiHeader.vue
@@ -1,6 +1,8 @@
 <template>
   <header>
-    
+    <div class="container">
+        <img  class="ms_header-logo" src="https://cantabrialabsdifacooper.it/wp-content/uploads/2021/03/png-clipart-spotify-logo-spotify-computer-icons-podcast-music-apps-miscellaneous-angle.png" alt="Logo">
+    </div>
   </header>
 </template>
 
@@ -10,6 +12,13 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+
+div.container{
+    width: 100%;
+    height: 80px;
+    background-color: black;
+}
+
 
 </style>
