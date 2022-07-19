66e19ea5ee3a2fe3595737ee066e0fc4a2173483
saff-it <saff_it@ymail.com>
2022-07-19 15:10:42 +0200

    Vue set up completed

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <spotiHeader />
+    <spotiMain />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import spotiHeader from './components/spotiHeader.vue'
+import spotiMain from './components/spotiMain.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    spotiHeader,
+    spotiMain,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+
+
+
 </style>
