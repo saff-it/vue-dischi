cdc6002bb6b2cdac7c45c3bd0be0c96ab8f4b0af
saff-it <saff_it@ymail.com>
2022-07-19 16:47:09 +0200

    Vue - discList components updates

--- src/components/discList.vue
+++ src/components/discList.vue
@@ -1,11 +1,14 @@
 <template>
 
-    <div class="ms_disc-container">
-        <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"
-            alt="Disc Cover">
-        <h3>New Jersey</h3>
-        <span>Bob Joviu <br> 1190</span>
-    </div>
+    <section>
+        <div class="ms_disc-container">
+            <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"
+                alt="Disc Cover">
+            <h3>New Jersey</h3>
+            <span>Bob Joviu <br> 1190</span>
+        </div>
+    </section>
+    
 
 </template>
 
@@ -15,5 +18,40 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import "../styles/general.scss";
+@import "../styles/variables.scss";
+
+section{
+    display: flex;
+    flex-wrap: wrap;
+}
+
+.ms_disc-container {
+    height: 350px;
+    width: calc(100% / 5 - 40px);
+    color: white;
+    text-align: center;
+    background-color: $cardColorBg;
+    padding: 18px;
+    margin: 20px;
+
+    img {
+        width: 90%;
+        height: 50%;
+        object-fit: cover;
+        object-position: top;
+    }
+
+    h3 {
+        text-transform: uppercase;
+        font-size: 1.2rem;
+        margin: 12px 0;
+    }
+
+    span {
+        color: rgb(104, 102, 102);
+    }
+
+}
 </style>
--- src/components/spotiMain.vue
+++ src/components/spotiMain.vue
@@ -1,10 +1,8 @@
 <template>
     <main>
         <div class="container">
-            <section>
-                <discList />
-
-            </section>
+            <discList />
+            
         </div>
     </main>
 </template>
@@ -33,37 +31,5 @@ div.container {
     margin-top: 70px; 
 }
 
-section{
-    display: flex;
-    flex-wrap: wrap;
-}
-
-div.ms_disc-container {
-    height: 350px;
-    width: calc(100% / 5 - 40px);
-    color: white;
-    text-align: center;
-    background-color: $cardColorBg;
-    padding: 18px;
-    margin: 20px;
-
-    img {
-        width: 90%;
-        height: 50%;
-        object-fit: cover;
-        object-position: top;
-    }
-
-    h3 {
-        text-transform: uppercase;
-        font-size: 1.2rem;
-        margin: 12px 0;
-    }
-
-    span {
-        color: rgb(104, 102, 102);
-    }
-
-}
 
 </style>
