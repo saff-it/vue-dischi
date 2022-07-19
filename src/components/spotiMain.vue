f133a7d85d05861021304cf495531b1769442709
saff-it <saff_it@ymail.com>
2022-07-19 16:42:28 +0200

    Vue - discList components is set up

--- src/components/spotiMain.vue
+++ src/components/spotiMain.vue
@@ -1,22 +1,16 @@
 <template>
     <main>
         <div class="container">
-
             <section>
-                <div class="ms_disc-container">
-                    <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"
-                        alt="Disc Cover">
-                    <h3>New Jersey</h3>
-                    <span>Bob Joviu <br> 1190</span>
-                </div>
-            </section>
+                <discList />
 
+            </section>
         </div>
     </main>
 </template>
 
 <script>
-import discList from './components/discList.vue'
+import discList from './discList.vue'
 
 
 
